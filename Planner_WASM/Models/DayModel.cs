f57c8b344ad63d9f112204e97668dab462104ddd
Dmnoiik <d.marszalkiewicz2000@gmail.com>
2025-05-15 18:24:40 +0200

    Cleaning up project. Working on form

---
+++ Planner_WASM/Models/DayModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Planner_WASM.Models
+{
+    public class DayModel
+    {
+        public DateOnly Date { get; set; }
+
+        public string Notes { get; set; }
+
+        public List<TimeSlotModel>? TimeSlots { get; set; }
+    }
+}
---
+++ Planner_WASM/Models/TimeSlotModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Planner_WASM.Models
+{
+    public class TimeSlotModel
+    {
+        public TimeSpan TimeFrom { get; set; }
+        public TimeSpan TimeTo { get; set; }
+
+        public ActivityType Activity { get; set; }
+
+    }
+
+}
+
+public enum ActivityType
+{
+    Work,
+    Study,
+    Fitness,
+    Chores,
+    Fun,
+    Social,
+    Sleep
+}
